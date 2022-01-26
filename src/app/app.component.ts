881078a4567874b05b712fadad74bc35cd5a991d
chewyt <yeetian.chew@gmail.com>
2022-01-26 15:16:21 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'workshop3-new';
+  form:FormGroup;
+  tomorrow=new Date();
+
+  taskFormControl  =new FormControl('',[Validators.required]);
+  priorityFormControl  =new FormControl('',[Validators.required]);
+  dueDateFormControl  =new FormControl('',[Validators.required]);
+
+  constructor(private fb: FormBuilder){
+    this.tomorrow.setDate(this.tomorrow.getDate()+1)
+    this.form = this.fb.group({
+      task: this.taskFormControl,
+      priority: this.priorityFormControl,
+      dueDate: this.dueDateFormControl
+    })
+  }
+  addToDo(){
+
+  }
 }
---
+++ src/app/todo.ts
@@ -0,0 +1,14 @@
+export class Todo {
+
+    constructor(
+        
+        public task: string,
+        public priority: string,
+        public dueDate:Date,
+        public taskId: string,
+        public status?:boolean
+    ){
+
+    }
+
+}
